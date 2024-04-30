4e832c552090b4c2ac9d3756b7a06fa4475e3b35
yuquan <2517330502@qq.com>
2024-05-01 03:24:53 +0800

    code final

--- MyDL/basics.py
+++ MyDL/basics.py
@@ -3,13 +3,13 @@ import MyDL
 from MyDL.data import *
 
 def train(model, criterion, optimizer, train_data, val_data, num_epochs=10,
-           batch_size=256, lambda_L2=0.001, path='model_params', 
+           batch_size=256, lambda_L2=0.001, model_path='model_params', result_path='results',
            continue_if_exists=False, calc_val_loss_every_iteration=False):
     model_name = 'MLP3_({},{})_{}_L2-{}_lr-{}'.format(model.hidden_size1, model.hidden_size2, model.activ_func, lambda_L2, optimizer.lr)
     continued_train = False
-    if os.path.exists(f'{path}/{model_name}.npz'):
+    if os.path.exists(f'{model_path}/{model_name}.npz'):
         print(f"Model already exists. Loading model...")
-        model.load(f'{path}/{model_name}.npz')
+        model.load(f'{model_path}/{model_name}.npz')
         if not continue_if_exists:
             print(f"Model loaded successfully.")
         else:
@@ -17,12 +17,10 @@ def train(model, criterion, optimizer, train_data, val_data, num_epochs=10,
         continued_train = True
     if continued_train and not continue_if_exists:
         print('Model is not going to be trained further as continue_if_exists is set to False.\n')
-        with np.load(os.path.join('results', f'{model_name}.npz')) as results:
-            train_loss = results['train_loss'].tolist()
-            val_loss = results['val_loss'].tolist()
-            train_acc = results['train_acc'].tolist()
-            val_acc = results['val_acc'].tolist()
-        return train_loss, val_loss, train_acc, val_acc, continued_train
+        result = np.load(f'{result_path}/{model_name}.npz', allow_pickle=True)
+        result = {key: result[key] for key in result}
+        result['continued_train'] = continued_train
+        return result
     train_loss_iter, val_loss_iter, train_acc_iter, val_acc_iter = [], [], [], []
     train_loss_epoch, val_loss_epoch, train_acc_epoch, val_acc_epoch = [], [], [], []
     for epoch in range(num_epochs):
@@ -36,7 +34,8 @@ def train(model, criterion, optimizer, train_data, val_data, num_epochs=10,
             train_loss_iter.append(loss.data)
             L2 = MyDL.MyTensor(0.)
             for param in model.params:
-                L2 = L2 + param.square().sum().item()
+                if param.requires_grad:
+                    L2 = L2 + param.square().sum().item()
             loss_with_L2 = loss + lambda_L2 * L2
             epoch_training_loss += loss.data * len(X_batch)
             y_pred = output.data.argmax(axis=1)
@@ -48,7 +47,7 @@ def train(model, criterion, optimizer, train_data, val_data, num_epochs=10,
             loss_with_L2.backward()
             optimizer.step()
             if calc_val_loss_every_iteration:
-                val_loss, val_acc = test(model, val_data, criterion, 4 * batch_size)
+                val_loss, val_acc = test(model, val_data, criterion, 4 * batch_size)  # Larger batch size for validation as it doesn't require backpropagation. Boosts speed.
                 val_loss_iter.append(val_loss)
                 val_acc_iter.append(val_acc)
         epoch_training_loss /= len(train_data)
@@ -61,9 +60,10 @@ def train(model, criterion, optimizer, train_data, val_data, num_epochs=10,
         val_acc_epoch.append(val_acc)
         spaces = len(f'Epoch {epoch + 1}/{num_epochs}.') * ' '
         print(f"{spaces} Validation Loss: {val_loss:.3f} \t Accuracy: {val_acc:.3f}")
-    model.save(filename=f'{model_name}.npz', path=path)
+    model.save(filename=f'{model_name}.npz', path=model_path)
     print('\n')
-    return {'train_loss_iter': train_loss_iter, 'val_loss_iter': val_loss_iter, 'train_acc_iter': train_acc_iter, 'val_acc_iter': val_acc_iter, 'train_loss_epoch': train_loss_epoch, 'val_loss_epoch': val_loss_epoch, 'train_acc_epoch': train_acc_epoch, 'val_acc_epoch': val_acc_epoch, 'continued_train': continued_train, 'batch_size': batch_size, 'model_name': model_name}
+    result = {'train_loss_iter': train_loss_iter, 'val_loss_iter': val_loss_iter, 'train_acc_iter': train_acc_iter, 'val_acc_iter': val_acc_iter, 'train_loss_epoch': train_loss_epoch, 'val_loss_epoch': val_loss_epoch, 'train_acc_epoch': train_acc_epoch, 'val_acc_epoch': val_acc_epoch, 'continued_train': continued_train, 'batch_size': batch_size, 'model_name': model_name}
+    return result
 
 
 def test(model, test_data, criterion, batch_size=256):
@@ -107,7 +107,8 @@ def save_result(train_loss_iter, train_acc_iter, train_loss_epoch, val_loss_epoc
         no_missing_val_loss_iter = True
     else:
         no_missing_val_loss_iter = False
-    np.savez(**{'file': path, 'train_loss_iter': train_loss_iter, 'val_loss_iter': val_loss_iter, 'train_acc_iter': train_acc_iter, 'val_acc_iter': val_acc_iter, 'train_loss_epoch': train_loss_epoch, 'val_loss_epoch': val_loss_epoch, 'train_acc_epoch': train_acc_epoch, 'val_acc_epoch': val_acc_epoch, 'batch_size_till_iter': batch_size_arr, 'no_missing_val_loss_iter': no_missing_val_loss_iter})
+    # np.savez(**{'file': path, 'train_loss_iter': train_loss_iter, 'val_loss_iter': val_loss_iter, 'train_acc_iter': train_acc_iter, 'val_acc_iter': val_acc_iter, 'train_loss_epoch': train_loss_epoch, 'val_loss_epoch': val_loss_epoch, 'train_acc_epoch': train_acc_epoch, 'val_acc_epoch': val_acc_epoch, 'batch_size_till_iter': batch_size_arr, 'no_missing_val_loss_iter': no_missing_val_loss_iter, 'model_name': model_name, 'continued_train': continued_train})
+    np.savez(path, train_loss_iter=train_loss_iter, val_loss_iter=val_loss_iter, train_acc_iter=train_acc_iter, val_acc_iter=val_acc_iter, train_loss_epoch=train_loss_epoch, val_loss_epoch=val_loss_epoch, train_acc_epoch=train_acc_epoch, val_acc_epoch=val_acc_epoch, batch_size_till_iter=batch_size_arr, no_missing_val_loss_iter=no_missing_val_loss_iter, model_name=model_name, continued_train=continued_train)
 
 
 def load_result(model_name, path='results'):
